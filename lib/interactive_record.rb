200b40f36540e1e522411a646cbef318f5cb0433
William Pannell <lpann5@gmail.com>
2020-07-27 19:47:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,35 +10,26 @@ class InteractiveRecord
 
   def self.column_names
     DB[:conn].results_as_hash = true
-
     table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name})")
     column_names = []
-
     table_columns.each do |col|
       column_names << col["name"]
     end
-
     column_names.compact
   end
 
-  def initialize(objects={})
-    objects.each do |k, v|
-      self.send("#{k}=", v)
+  def initialize(objects_hash={})
+    objects_hash.each do |key, value|
+      self.send("#{key}=", value)
     end
   end
-
-  # ORM Methods
-
-  # SAVE - INSERT INTO students(name, grade) VALUES (x, y)
 
   def save
     sql = <<-SQL
       INSERT INTO #{table_name_for_insert}(#{col_names_for_insert})
       VALUES (#{values_for_insert})
       SQL
-
     DB[:conn].execute(sql)
-
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
@@ -52,7 +43,6 @@ class InteractiveRecord
 
   def values_for_insert
     values = []
-
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
@@ -64,19 +54,16 @@ class InteractiveRecord
       SELECT * FROM #{table_name}
       WHERE name = ?
       SQL
-
     DB[:conn].execute(sql, name);
   end
 
   def self.find_by(attribute)
     column_name = attribute.keys[0].to_s
     value_name = attribute.values[0]
-
     sql = <<-SQL
       SELECT * FROM #{table_name}
       WHERE #{column_name} = ?
       SQL
-
     DB[:conn].execute(sql, value_name);
   end
 
